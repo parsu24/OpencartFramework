0bb257b419b3d77309da7edeec352e2a6e6b6c8f
prashant <prashantbicky@gmail.com>
2026-08-31 16:17:47 +0530

    Resolve merge conflicts

--- config.py
+++ config.py
@@ -1,12 +1,6 @@
 class Config:
-<<<<<<< Updated upstream
     email="bicky@ai.com"
     password="test123"
-=======
-    email="pavanol@gi.com"
-    password="test@123"
->>>>>>> Stashed changes
-
     invalid_email="pavanol123@abc.com"
     invalid_password="test@123xyz"
     product_name = "MacBook"
